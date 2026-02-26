ec951c5f2d23e639eabbd1a4e908111816abdd87
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-26 13:19:45 +0600

    default parameters

--- default-para.js
+++ default-para.js
@@ -8,5 +8,26 @@ add(10)
 
 
 
-
 //  f 
+
+function add2(num1 , num2 = 0){
+const sum = num1 + num2;
+console.log(num1,num2, sum);
+
+}
+add2(10, 90);
+
+
+function fullName(first , last = ''){
+  const name = first +" "+ last;
+  console.log(name);
+
+}
+fullName('shoaib', 'rahman')
+
+
+function multiply(num1, num2= 1){
+const result = num1  * num2
+console.log(result);
+}
+multiply(5)
