76ae044e86aaaee05c0e36ec3678df204170d7d5
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-02-26 13:58:15 +0600

    learning spread Operator

---
+++ spread.js
@@ -0,0 +1,6 @@
+const max = Math.max(1,2,3,54,9,6,79,5,6,5,4,54);
+// console.log(max);
+
+const numbers = [2,3,4,5,6,77,88,969,61466,333,2222]
+const max2 = Math.max(...numbers)
+console.log(...numbers);
